faf0cecd78580268bb36a5b5c5c975a46846915c
zhengjie.shen <zhengjie.shen@iquantex.com>
2020-04-07 22:05:58 +0800

    feat: upgrade phoenix version dev-SNAPSHOT

--- domain/src/main/java/com/iquantex/phoenix/risk/domain/entity/HelloAggregate.java
+++ domain/src/main/java/com/iquantex/phoenix/risk/domain/entity/HelloAggregate.java
@@ -1,10 +1,10 @@
 package com.iquantex.phoenix.risk.domain.entity;
 
 import com.iquantex.phoenix.risk.coreapi.Hello;
-import com.iquantex.phoenix.server.aggregate.entity.ActReturn;
-import com.iquantex.phoenix.server.aggregate.entity.AggregateRootIdAnnotation;
+import com.iquantex.phoenix.server.aggregate.entity.CommandHandler;
 import com.iquantex.phoenix.server.aggregate.entity.EntityAggregateAnnotation;
-import com.iquantex.phoenix.server.aggregate.entity.RetCode;
+import com.iquantex.phoenix.server.aggregate.model.ActReturn;
+import com.iquantex.phoenix.server.aggregate.model.RetCode;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,12 +28,11 @@ public class HelloAggregate implements Serializable {
 	 * @param cmd
 	 * @return
 	 */
-	@AggregateRootIdAnnotation(aggregateRootId = "helloId")
+	@CommandHandler(aggregateRootId = "helloId")
 	public ActReturn act(Hello.HelloCmd cmd) {
 		log.info("Hello World Phoenix...");
-		return ActReturn
-				.builder(RetCode.SUCCESS, "success", Hello.HelloEvent.newBuilder().setHelloId(cmd.getHelloId()).build())
-				.build();
+		return ActReturn.builder().retCode(RetCode.SUCCESS).retMessage("success")
+				.event(Hello.HelloEvent.newBuilder().setHelloId(cmd.getHelloId()).build()).build();
 	}
 
 	/**
